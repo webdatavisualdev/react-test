7fa64b313acb94830d55eeec7b0516774067fa27
webdatavisualdev <cli698638@gmail.com>
2022-03-03 09:20:27 -0800

    Updated UIs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
+import react from "react";
+
+import ProductCard from "./components/ProductCard";
 
 // Use this ID to fetch the correct product from the API
 // eslint-disable-next-line
 const PRODUCT_ID = "1";
 
 function App() {
-  return <div>App goes here</div>;
+  return <ProductCard prodcutId={PRODUCT_ID} />;
 }
 
 export default App;
